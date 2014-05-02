0c89e033f3f3783caee7345bc25071a1cf385fe7
Yannick Schutz <yannick.schutz@gmail.com>
2014-05-03 00:07:01 +0200

    Use struct in server too.

--- server/server.go
+++ server/server.go
@@ -5,21 +5,29 @@ import (
 	"github.com/ys/kanejaku/metric"
 )
 
-func Run() {
-	metrics().Run()
+type Server interface {
+	Run()
 }
 
-func metrics() bourbon.Bourbon {
+type DefaultServer struct {
+	Store metric.Store
+}
+
+func (s *DefaultServer) Run() {
+	s.metrics().Run()
+}
+
+func (s *DefaultServer) metrics() bourbon.Bourbon {
 	b := bourbon.New()
 	b.Get("/metrics", func() (int, bourbon.Encodeable) {
 		return 418, "TEAPOT"
 	})
 	b.Post("/metrics", func(metrics []metric.Metric) (int, bourbon.Encodeable) {
-		metric.AddMany(metrics)
+		s.Store.AddMany(metrics)
 		return 201, metrics
 	})
 	b.Get("/metrics/{key}", func(params bourbon.Params) (int, bourbon.Encodeable) {
-		return 200, metric.Get(params["key"])
+		return 200, s.Store.Get(params["key"])
 	})
 	return b
 }
