d838f109ef501b571287bad43ca9c4079691d28c
Yannick Schutz <yannick.schutz@gmail.com>
2014-05-03 23:27:36 +0200

    Add functions to schema and drop unique index

--- schema.sql
+++ schema.sql
@@ -7,4 +7,77 @@ CREATE TABLE metrics(
   timestamp bigint
 );
 
-CREATE UNIQUE INDEX index_on_metrics_for_key_and_timestamp ON metrics(key, timestamp);
+CREATE INDEX index_on_metrics_for_key_and_timestamp ON metrics(key, timestamp);
+
+-- Found on https://stackoverflow.com/questions/14300004/postgresql-equivalent-of-oracles-percentile-cont-function/14309370#14309370
+
+CREATE OR REPLACE FUNCTION array_sort (ANYARRAY)
+RETURNS ANYARRAY LANGUAGE SQL
+AS $$
+SELECT ARRAY(
+  SELECT $1[s.i] AS "foo"
+  FROM
+  generate_series(array_lower($1,1), array_upper($1,1)) AS s(i)
+  ORDER BY foo
+);
+$$;
+
+CREATE OR REPLACE FUNCTION percentile_cont(myarray float[], percentile float)
+RETURNS real AS
+$$
+
+DECLARE
+ary_cnt INTEGER;
+row_num float;
+crn float;
+frn float;
+calc_result float;
+new_array float[];
+BEGIN
+  ary_cnt = array_length(myarray,1);
+  row_num = 1 + ( percentile * ( ary_cnt - 1 ));
+  new_array = array_sort(myarray);
+
+  crn = ceiling(row_num);
+  frn = floor(row_num);
+
+  if crn = frn and frn = row_num then
+    calc_result = new_array[row_num];
+  else
+    calc_result = (crn - row_num) * new_array[frn]
+    + (row_num - frn) * new_array[crn];
+  end if;
+
+  RETURN calc_result;
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
+
+
+CREATE FUNCTION _final_median(anyarray) RETURNS float8 AS $$
+WITH q AS
+(
+   SELECT val
+   FROM unnest($1) val
+   WHERE VAL IS NOT NULL
+   ORDER BY 1
+),
+cnt AS
+(
+  SELECT COUNT(*) AS c FROM q
+)
+SELECT AVG(val)::float8
+FROM
+(
+  SELECT val FROM q
+  LIMIT  2 - MOD((SELECT c FROM cnt), 2)
+  OFFSET GREATEST(CEIL((SELECT c FROM cnt) / 2.0) - 1,0)
+) q2;
+$$ LANGUAGE sql IMMUTABLE;
+
+CREATE AGGREGATE median(anyelement) (
+SFUNC=array_append,
+STYPE=anyarray,
+FINALFUNC=_final_median,
+INITCOND='{}'
+);
