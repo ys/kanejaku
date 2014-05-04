e0aac6e77466ae61cae20a914a92811314bf4d26
Yannick Schutz <yannick.schutz@gmail.com>
2014-05-04 23:51:02 +0200

    Store in timestamptz column

--- metric/metric.go
+++ metric/metric.go
@@ -1,15 +1,15 @@
 package metric
 
 import (
-	"fmt"
 	"log"
 	"time"
 )
 
 type Metric struct {
-	Key       string  `json:"key",db:"key"`
-	Value     float32 `json:"value",db:"value"`
-	Timestamp int64   `json:"timestamp",db:"timestamp"`
+	Key          string     `json:"key"`
+	Value        float32    `json:"value"`
+	Timestamp    *time.Time `json:"-"`
+	TimestampInt int64      `json:"timestamp"`
 }
 
 func (s *DefaultStore) AddMany(metrics []Metric) {
@@ -19,10 +19,13 @@ func (s *DefaultStore) AddMany(metrics []Metric) {
 }
 
 func (s *DefaultStore) Add(m Metric) {
-	if m.Timestamp == 0 {
-		m.Timestamp = time.Now().UTC().Unix()
+	if m.TimestampInt == 0 {
+		if m.Timestamp == nil || m.Timestamp.IsZero() {
+			t := time.Now().UTC()
+			m.Timestamp = &t
+		}
+		m.TimestampInt = m.Timestamp.Unix()
 	}
-	fmt.Println(time.Unix(m.Timestamp, 0))
 	sStmt := "insert into metrics(key, value, timestamp) values ($1, $2, $3)"
 	stmt, err := s.DB.Prepare(sStmt)
 	defer stmt.Close()
@@ -57,12 +60,12 @@ func (s *DefaultStore) Get(key string, function string, resolution int) []Metric
                                          ELSE avg(value)
                                          END
                                         ) AS value,
-                                        (ROUND(timestamp / $3) * $3)::bigint as timestamp
+                                        round_timestamp(timestamp, $3) as timestamp
                                FROM metrics
                                WHERE key = $1
-                               AND extract(epoch from (now() - interval '1 hour')) < timestamp
-                               GROUP BY (ROUND(timestamp / $3) * $3)
-                               ORDER BY (ROUND(timestamp / $3) * $3) DESC`, key, function, resolution)
+                               AND (now() - interval '1 hour') < timestamp
+                               GROUP BY round_timestamp(timestamp, $3)
+                               ORDER BY timestamp DESC`, key, function, resolution)
 	if err != nil {
 		log.Println(err)
 		return nil
@@ -72,6 +75,7 @@ func (s *DefaultStore) Get(key string, function string, resolution int) []Metric
 		if err := rows.Scan(&m.Key, &m.Value, &m.Timestamp); err != nil {
 			log.Fatal(err)
 		}
+		m.TimestampInt = m.Timestamp.Unix()
 		result = append(result, m)
 	}
 	return result
